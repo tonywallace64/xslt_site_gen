f7563dfe23dad089e7f4e635dab14dacbbee6b9c
Tony Wallace <tony@tony.gen.nz>
2016-06-09 21:28:28 +1200

    added comments

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 cd site_data
+# build site by file inclusion
 escript ../xslt_extn < pagedata.xml > fullsite.xml 2> errors.txt
-xsltproc ../src/make_web2.xsl fullsite.xml > allpages.xml
+# transform by xslt
+xsltproc ../src/make_web.xsl fullsite.xml > allpages.xml
+# split output files
 escript ../xslt_extn +finaloutput < allpages.xml 
-mv *.html ../html
+# put results
+mv *.xhtml ../html
